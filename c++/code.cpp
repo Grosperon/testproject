296a52b5c801fdd189e558467aa7c7efdaed713f
Angelina Shishkina <angy2788@mail.ru>
2024-05-15 11:29:51 +0300

    wow

--- c++/code.cpp
+++ c++/code.cpp
@@ -4,8 +4,8 @@ using namespace std;
 struct Node
 {
     int data;
-    Node *left;//все меньшие
-    Node *right;//все большие
+    Node *left; // все меньшие
+    Node *right; // все большие
 };
 
 class tree{
@@ -14,7 +14,7 @@ private:
     int length;
     int hight;
 public:
-    tree() //конструктор класса без параметров
+    tree() // конструктор класса без параметров
     {
         head = NULL;
         length = 0;
@@ -22,93 +22,99 @@ public:
     }
 
     void addNode(int d){
-        length ++;//количество ячеек
+        length++;
         if(head == NULL)  {
+            head = new Node;
             head->data = d;
             head->left = NULL;
-            head->right = NULL;  //если создаем первый узел
-            }
+            head->right = NULL;
+        }
         else
         {
-            Node *nd = new Node;
-            nd = head;
+            Node *nd = head;
             while (nd != NULL ){
-            if(d>nd->data) nd = nd->right;
-            else nd = nd->left;
+                if(d > nd->data) {
+                    if(nd->right == NULL) {
+                        nd->right = new Node;
+                        nd->right->data = d;
+                        nd->right->left = NULL;
+                        nd->right->right = NULL;
+                        break;
+                    } else {
+                        nd = nd->right;
+                    }
+                } else {
+                    if(nd->left == NULL) {
+                        nd->left = new Node;
+                        nd->left->data = d;
+                        nd->left->left = NULL;
+                        nd->left->right = NULL;
+                        break;
+                    } else {
+                        nd = nd->left;
+                    }
+                }
             }
-            nd->data = d;
-            nd->left = NULL;
-            nd->right = NULL;
         }
-}
-
-
+    }
 
     void poisk(int z){
-        Node *nd = new Node;
-        nd = head;
-        while(z != nd->data){
-            if(nd->left == NULL && nd->right == NULL) {
-                cout<<"no"<<endl;
-                break; }
+        Node *nd = head;
+        while(nd != NULL && z != nd->data){
+            if(z > nd->data)
+                nd = nd->right;
             else
-            if(z>nd->data) nd = nd->right;
-            else nd = nd->left;
-            if(z == nd->data) cout<<"yes"<<endl;
-            }
-    }
-    void LLL(){cout<<length<<" "<<hight<<endl;}//количество чисел в дереве
-//    void printList()//вывод списка
-//    {
-//        Node *c = head;
-//
-//        while(c != NULL)
-//        {
-//            cout << c->data<<" ";
-//            c = c->next;
-//        }
-//        cout<< endl;
-//    }
-    void prefix(){ //префикснаня форма обхода дерева
-    if( tree != NULL )
-    {
-        cout<< tree -> data;
-        prefix(tree -> left);
-        prefix(tree -> right);
+                nd = nd->left;
         }
 
-
+        if(nd == NULL)
+            cout << "no" << endl;
+        else
+            cout << "yes" << endl;
     }
-    void infix(){
-    if(tree != NULL)
-    {
-        infix(tree -> left);
-        cout<< tree -> data;
-        infix(tree -> right);
+
+    void LLL() {
+        cout << length << " " << hight << endl;
+    }
+
+    void prefix(Node* tree) {
+        if (tree != NULL)
+        {
+            cout << tree->data;
+            prefix(tree->left);
+            prefix(tree->right);
+        }
+    }
+
+    void infix(Node* tree) {
+        if (tree != NULL)
+        {
+            infix(tree->left);
+            cout << tree->data;
+            infix(tree->right);
 
         }
-
-    }
-    void postfix() {
-    if(tree != NULL)
-    {
-        postfix(tree -> left);
-        postfix(tree -> right);
-        cout<< tree -> data;
     }
 
-        };
+    void postfix(Node* tree) {
+        if (tree != NULL)
+        {
+            postfix(tree->left);
+            postfix(tree->right);
+            cout << tree->data;
+        }
+    }
+};
+
 int main()
 {
     tree A;
-    int n;
-    cin>>n;
-    for(int i =0; i<n; i++){
-        int a;
-        cin>>a;
-        cout<<1;
+    int n, a;
+    cin >> n;
+    for(int i = 0; i < n; i++){
+        cin >> a;
         A.addNode(a);
-        }
+    }
 
-
+    return 0;
 }
